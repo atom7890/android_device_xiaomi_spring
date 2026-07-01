6f2f07f02d58a08aa738d9a8d130469fd4ae9946
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-07-01 21:54:47 +0530

    spring: Fix Dolby Spatial Audio output on `Bluetooth,Wired,USB`
    Change-Id: Ie5dab06116e6a38bad99d5d3ee09ca693cbbb5fd
    Signed-off-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -79,7 +79,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_blair/audio_effects.xml \
-    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_blair/audio_policy_configuration.xml
+    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_blair/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
